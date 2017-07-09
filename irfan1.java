4f06c48d370d867208ed7706a2656d43eeaec1ed
mohamed-irfan24 <mohamed_irfan@srmuniv.edu.in>
2017-07-09 14:59:39 +0530

    Create irfan1.java

---
+++ irfan1.java
@@ -0,0 +1,25 @@
+int a=25253;
+int k=a-25250;
+System.out.println(+k);
+
+int a=-2234
+int k=2234+a;
+System.out.println(+k);
+
+nt n;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the number you want to check:");
+        n = s.nextInt();
+        if(n > 0)
+        {
+            System.out.println("The given number "+n+" is Positive");
+        }
+        else if(n < 0)
+        {
+            System.out.println("The given number "+n+" is Negative");
+        }
+        else
+        {
+            System.out.println("The given number "+n+" is neither Positive nor Negative ");
+        }
+    }
